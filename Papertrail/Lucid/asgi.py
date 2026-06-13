63f97f4daabe68cb93ac10c7932bc24574ed8301
Kelly Mascarenhas <kellymascarenhas@Kellys-MacBook-Air-4.local>
2026-06-13 12:18:26 +1200

    Renamed project from 'Lucid' to 'Papertrail'. Updated all occurences of 'Lucid' to 'Papertrail'

---
+++ Papertrail/Lucid/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Papertrail project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Papertrail.settings')
+
+application = get_asgi_application()

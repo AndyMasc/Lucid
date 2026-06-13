a328cb0fe901b45bf875ac423840fd32004c2110
Kelly Mascarenhas <kellymascarenhas@Kellys-MacBook-Air-4.local>
2026-06-13 13:18:20 +1200

    Added django tests for Homepage - Fixed app name error in urls.py (changed app name from 'home' to   'core') - Added django tests for homepage, including template rendering test,   and home status test. - Renamed homepage function from 'home' to 'index'

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class HomeViewTest(TestCase):
+    def test_home_status(self):
+        response = self.client.get(reverse("core:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_template(self):
+        response = self.client.get(reverse("core:home"))
+        self.assertTemplateUsed(response, 'core/home.html')
